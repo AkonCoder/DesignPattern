648c3fbe8a6db5ad3007e58f4ef104f1ac7dbc41
liupeng <lipeng_internet@163.com>
2017-02-13 14:27:31 +0800

    Add: project initial

---
+++ SimpleFactoryPattern/Operation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactoryPattern
+{
+    /// <summary>
+    /// 运算基类
+    /// </summary>
+    public class Operation
+    {
+        private double _numberA = 0;
+        private double _numberB = 0;
+
+
+        public double numberA
+        {
+            get { return _numberA; }
+            set { _numberA = value; }
+        }
+
+        public double numberB
+        {
+            get { return _numberB; }
+            set { _numberB = value; }
+        }
+
+        public virtual double GetOperstionResult()
+        {
+            double result = 0;
+            return result;
+        }
+    }
+}
---
+++ SimpleFactoryPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactoryPattern
+{
+    /// <summary>
+    /// 简单工厂模式
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+        }
+    }
+}
